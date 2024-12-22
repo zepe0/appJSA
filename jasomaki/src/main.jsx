80eda079e9b9e993399c379912e7d921afc16b5b
zepe0 <aitor.zepe@gmail.com>
2024-12-22 22:43:57 +0100

    Actualizar enrutamiento en main.jsx para mostrar HomeUser y agregar configuración de reescritura en vercel.json

--- jasomaki/src/main.jsx
+++ jasomaki/src/main.jsx
@@ -3,18 +3,20 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { RouterProvider,createHashRouter } from "react-router-dom";
-
+import HomeUser from "../pages/Prueva.jsx";
 const Router = createHashRouter([
   {
-    path: "/*",
-    element: <App />,
+    path: "/",
+    element: <HomeUser />,
   }
 ]);
+
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={Router}>
-      <App />
+  
     </RouterProvider>
   </StrictMode>
 );
