6d9fe43d778cffb2a6fd87db8500af7e7949d317
zepe0 <aitor.zepe@gmail.com>
2024-12-22 19:44:58 +0100

    Agregar enrutamiento básico con React Router en main.jsx

--- jasomaki/src/main.jsx
+++ jasomaki/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider,createHashRouter } from "react-router-dom";
+import HomeUser from "../pages/Prueva.jsx";
+const Router = createHashRouter([
+  {
+    path: "/",
+    element: <HomeUser />,
+  }
+]);
 
-createRoot(document.getElementById('root')).render(
+
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={Router}>
+  
+    </RouterProvider>
+  </StrictMode>
+);
